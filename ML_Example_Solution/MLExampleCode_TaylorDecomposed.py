711814dd32c844fdccb37f756c8f94bef34becc7
Daniyal Saleem <daniyalsaleem.d@gmail.com>
2020-12-31 05:42:16 +0100

    refactor

--- ML_Example_Solution/MLExampleCode_TaylorDecomposed.py
+++ ML_Example_Solution/MLExampleCode_TaylorDecomposed.py
@@ -1,39 +1,20 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 
 # # Input data
 X = np.array([0.0,0.0,0.0])
 
-
-# In[15]:
-
-
-# Simple neural-network based regressor
 class NeuralNetwork:
-# Function called at object initialization
     def __init__(self):
 
         # These are members of the class. We can access them in every method by \"self.var_name\" 
         #and from outside the class with \"instance_name.var_name\"
 
-        # Sample to compute pass with # Inputs
-        self.X = 0.0 # 1st input # will be input by user 
-        self.y = 0.0 # set 2nd input # will be input by user
-        self.z = 0.0 # set 3rd input # will be input by user
-        
-        self.X_new = 0.0 # new 1st input # will be calculated 
-        self.y_new = 0.0 # new 2nd input # will be calculated
-        self.z_new = 0.0 # new 2nd input # will be calculated
-        
+        self.inputs = {"X": 0.0, "y": 0.0, "z": 0.0} # input # will be input by user 
+        self.inputs_new = {"X": 0.0, "y": 0.0, "z": 0.0} # new input # will be calculated         
         self.goalOutput = 0.0 # set goalOutput # will be input by user
         
-        # Distribution percentages
+        # Neuron Backward Distribution percentages
         self.PL1_N1 = 0.688 # % to distribut main output (L1) to newron_1 (N1) of prev layer 
         self.PL1_N2 = 0.312 # % to distribut main output (L1) to newron_2 (N2) of prev layer
         
@@ -84,31 +65,28 @@ class NeuralNetwork:
 
     # Set sample to be used in feed-forward and back-propagation pass
     def set_sample(self, X, y, z, goalOutput):
-        self.X = float(X)
-        self.y = float(y)
-        self.z = float(z)
+        self.inputs["X"] = float(X)
+        self.inputs["y"] = float(y)
+        self.inputs["z"] = float(z)
         self.goalOutput = float(goalOutput)
 
     ## Feed-forward pass
     def feed_forward(self):
-        net_hidden_11 = (self.X * self.weight_11) + self.bias_11
-        net_hidden_21 = (self.y * self.weight_21) + self.bias_21
-        net_hidden_31 = (self.z * self.weight_31) + self.bias_31
+        net_hidden_11 = (self.inputs["X"] * self.weight_11) + self.bias_11
+        net_hidden_21 = (self.inputs["y"] * self.weight_21) + self.bias_21
+        net_hidden_31 = (self.inputs["z"] * self.weight_31) + self.bias_31
         self.out_hidden_1 = ReLu(net_hidden_11+net_hidden_21+net_hidden_31) #ReLu function // output 1st neuron of Hidden layer 1
         print("Value of Hidden Neuron_1: " + str(round(self.out_hidden_1,3)))
         
-        net_hidden_12 = (self.X * self.weight_12) + self.bias_12
-        net_hidden_22 = (self.y * self.weight_22) + self.bias_22
-        net_hidden_32 = (self.z * self.weight_32) + self.bias_32
+        net_hidden_12 = (self.inputs["X"] * self.weight_12) + self.bias_12
+        net_hidden_22 = (self.inputs["y"] * self.weight_22) + self.bias_22
+        net_hidden_32 = (self.inputs["z"] * self.weight_32) + self.bias_32
         self.out_hidden_2 = ReLu(net_hidden_12+net_hidden_22+net_hidden_32) #ReLu function // output 2nd neuron of Hidden layer 1
         print("Value of Hidden Neuron_2: " + str(round(self.out_hidden_2,3)))
 
         net_out_1 = (self.out_hidden_1  * self.weight_l21) + self.bias_l21
         net_out_2 = (self.out_hidden_2  * self.weight_l22) + self.bias_l22
         
-#         print("net_out_1 : " + str(net_out_1))
-#         print("net_out_2 : " + str(net_out_2))
-
         self.output = sigmoid(net_out_1+net_out_2) #sigmoid function // Predicted Output
 
     ## Back-propagation for Taylor Decomposition and Find new Input Values
@@ -119,19 +97,18 @@ class NeuralNetwork:
                 
         if(self.out_hidden_1<0 or self.out_hidden_2<0):
             return False
-        else:
-            X_new_a = self.X - (((self.out_hidden_1 - self.out_hidden_1_new) * self.PN1_I1) / (self.weight_11))
-            y_new_a = self.y - (((self.out_hidden_1 - self.out_hidden_1_new) * self.PN1_I2) / (self.weight_21))
-            z_new_a = self.z - (((self.out_hidden_1 - self.out_hidden_1_new) * self.PN1_I3) / (self.weight_31))
+        else:            
+            X_new_a = self.inputs["X"] - (((self.out_hidden_1 - self.out_hidden_1_new) * self.PN1_I1) / (self.weight_11))
+            y_new_a = self.inputs["y"] - (((self.out_hidden_1 - self.out_hidden_1_new) * self.PN1_I2) / (self.weight_21))
+            z_new_a = self.inputs["z"] - (((self.out_hidden_1 - self.out_hidden_1_new) * self.PN1_I3) / (self.weight_31))
 
-            X_new_b = self.X - (((self.out_hidden_2 - self.out_hidden_2_new) * self.PN2_I1) / (self.weight_12))
-            y_new_b = self.y - (((self.out_hidden_2 - self.out_hidden_2_new) * self.PN2_I2) / (self.weight_22))
-            z_new_b = self.z - (((self.out_hidden_2 - self.out_hidden_2_new) * self.PN2_I3) / (self.weight_32))
+            X_new_b = self.inputs["X"] - (((self.out_hidden_2 - self.out_hidden_2_new) * self.PN2_I1) / (self.weight_12))
+            y_new_b = self.inputs["y"] - (((self.out_hidden_2 - self.out_hidden_2_new) * self.PN2_I2) / (self.weight_22))
+            z_new_b = self.inputs["z"] - (((self.out_hidden_2 - self.out_hidden_2_new) * self.PN2_I3) / (self.weight_32))
 
-            self.X_new = X_new_a + X_new_b 
-            self.y_new = y_new_a + y_new_b
-            self.z_new = z_new_a + z_new_b
-
+            self.inputs_new["X"] = X_new_a + X_new_b 
+            self.inputs_new["y"] = y_new_a + y_new_b
+            self.inputs_new["z"] = z_new_a + z_new_b
             return True
         
 # Sigmoid Function
@@ -145,7 +122,6 @@ def ReLu(s):
 def execute_nn(X, y, z, goalOutput):
     nn = NeuralNetwork() # Instantiate neural network
     nn.set_sample(X, y, z, goalOutput) # set input values
-    
     nn.feed_forward() # perform feed-forward to calculate output        
     print("\nFinal Output: "+ str(round(nn.output,3)) + "\n")
     
@@ -154,9 +130,9 @@ def execute_nn(X, y, z, goalOutput):
     if(isDecomposable):        
         print("New Value of Hidden Neuron 1: "+ str(round(nn.out_hidden_1_new,3)))
         print("New Value of Hidden Neuron 2: "+ str(round(nn.out_hidden_2_new,3)) + "\n")
-        print("New first Input: "+ str(round(nn.X_new,3)))
-        print("New 2nd Input: "+ str(round(nn.y_new,3)))
-        print("New 3rd Input: "+ str(round(nn.z_new,3)))
+        print("New first Input: "+ str(round(nn.inputs_new["X"],3)))
+        print("New 2nd Input: "+ str(round(nn.inputs_new["y"],3)))
+        print("New 3rd Input: "+ str(round(nn.inputs_new["z"],3)))
     else:
         print("-ve values encountered for ReLU. Please select different % Distributions")
 
